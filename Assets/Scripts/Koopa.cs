293e38338eb27f1cbe49791fd4fe8a90eaf3e93b
Malchio03 <malchio1753@gmail.com>
2024-08-21 14:16:39 +0200

    Added Koopa and fixed Goomba

---
+++ Assets/Scripts/Koopa.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Koopa : MonoBehaviour
+{
+    public Sprite shellSprite;
+
+    private bool shelled;
+    private bool shellMoving;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!shelled && collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            if (collision.transform.DotTest(transform, Vector2.down))
+            {
+                EnterShell();
+            }
+            else
+            {
+                player.Hit();
+            }
+        }
+    }
+
+    private void EnterShell()
+    {
+        shelled = true;
+        GetComponent<EntityMovement>().enabled = false;
+        GetComponent<AnimationsSprites>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = shellSprite;
+    }
+}
