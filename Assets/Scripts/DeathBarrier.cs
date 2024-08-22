7da0920252047900a3684c479ff45d66ac5c6bfe
Malchio03 <malchio1753@gmail.com>
2024-08-22 13:59:15 +0200

    Added deathbarrier to scene

--- Assets/Scripts/DeathBarrier.cs
+++ Assets/Scripts/DeathBarrier.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DeathBarrier : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+            GameManager.Instance.ResetLevel(3f);
+        } else
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
