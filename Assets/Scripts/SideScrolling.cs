b5326021fb4a6973f54d435aec225e3f1f92037d
Malchio03 <malchio1753@gmail.com>
2024-10-24 10:28:12 +0200

    SetUnderGroundCamera

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
 // Questo script gestisce il side-scrolling della telecamera o di un altro oggetto
-// seguendo la posizione del giocatore in un gioco 2D.
+// seguendo la posizione del giocatore
 
 public class SideScrolling : MonoBehaviour
 {
     private Transform player;
+
+    public float height = 6.5f;
+    public float undergroundHeight = -9.5f;
 
     private void Awake() {
         player = GameObject.FindWithTag("Player").transform;
@@ -16,5 +19,11 @@ public class SideScrolling : MonoBehaviour
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
         transform.position = cameraPosition;
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
     }
 }
